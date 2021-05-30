e9351c231c038773d7e8e61d0643a15b5cf8e067
Sanzhar <190103205@stu.sdu.edu.kz>
2021-05-30 12:11:15 +0600

    added service&repository interf, config, viper

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,28 @@ import (
 
 	"github.com/h-u-m-a-n/todo-app"
 	"github.com/h-u-m-a-n/todo-app/package/handler"
+	"github.com/h-u-m-a-n/todo-app/package/repository"
+	"github.com/h-u-m-a-n/todo-app/package/service"
+	"github.com/spf13/viper"
 )
 
 
 func main() {
-	handlers := new(handler.Handler)
-	
+	if err := initConfig(); err != nil{
+		log.Fatalf("error initialization configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil{
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil{
 		log.Fatalf("error occuring while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("configs/config.yml")
+	return viper.ReadInConfig()
 }
--- server.go
+++ server.go
@@ -14,7 +14,7 @@ func(s* Server) Run(port string, handler http.Handler) error{
 	s.httpServer = &http.Server{
 		Addr: ":" + port,
 		Handler: handler,
-		MaxHeaderBytes: 1<<20,
+		MaxHeaderBytes: 1<<20, // 1MB
 		ReadTimeout: 10*time.Second,
 		WriteTimeout: 10*time.Second,
 	}
