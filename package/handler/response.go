eda5ffdbe69c0e1c521b122fea9c292ef67af13f
Sanzhar <190103205@stu.sdu.edu.kz>
2021-06-10 16:48:21 +0600

    added create and read logic for lists

--- package/handler/response.go
+++ package/handler/response.go
@@ -6,11 +6,11 @@ import (
 )
 
 
-type error struct {
+type errorResponse struct {
 	Message string `json:"message"`
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Error(message)
-	c.AbortWithStatusJSON(statusCode,error{message})
+	c.AbortWithStatusJSON(statusCode,errorResponse{message})
 }
